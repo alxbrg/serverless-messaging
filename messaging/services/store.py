975c59752fc76eb54725a308e825ecc64cbab12c
Alexandre Borrego <alex@borrego.io>
2019-04-17 15:09:44 +0200

    add dummy create function to store service

--- messaging/services/store.py
+++ messaging/services/store.py
@@ -1,2 +1,6 @@
 def find_by_recipient(recipient):
     return [{'recipient': recipient}]
+
+
+def create(recipient, body, email_address=None, phone_number=None):
+    return {'recipient': recipient}
