432d3a9a5b28fad14017d92cf4252801a3253767
Alexandre Borrego <alex@borrego.io>
2019-04-17 14:42:33 +0200

    implement validation helpers

---
+++ messaging/helpers/validate.py
@@ -0,0 +1,25 @@
+from .errors import (ApiError, ErrorCodes)
+
+
+def params(predicates, data):
+    """
+    Checks the validity of params using a dictionary of predicates.
+
+    Args:
+        predicates (dict): A dictionary of predicate functions.
+        data (dict): data to validate
+
+    Returns:
+        None if all params are valid, raises a 400 ApiError otherwise
+    """
+    for key, predicate in predicates.items():
+        if not predicate(data.get(key)):
+            raise ApiError(
+                f"Missing or invalid '{key}' parameter",
+                400,
+                ErrorCodes.INVALID_PARAMETER,
+            )
+
+
+def is_non_empty_string(value):
+    return isinstance(value, str) and value.strip() != ''
