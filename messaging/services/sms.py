42d6c22021b41c57925f4862a246596e16081a1d
Alexandre Borrego <alex@borrego.io>
2019-04-17 16:21:35 +0200

    implement sms service

--- messaging/services/sms.py
+++ messaging/services/sms.py
@@ -1,2 +1,10 @@
+import os
+
+import boto3
+
+REGION = os.environ.get('AWS_REGION')
+
+
 def send(phone_number, body):
-    return None
+    client = boto3.client('sns', region_name=REGION)
+    client.publish(PhoneNumber=phone_number, Message=body)
